208fd02c4a6cc6ef42ed0a46c2e0245ddbc6afef
Vadim Filimonov <filimonov@duck.com>
2026-03-01 14:55:38 +0300

    Add import config

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
+import pluginImport from 'eslint-plugin-import';
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
@@ -24,6 +25,38 @@ export default defineConfig([
     },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+    },
+  },
+    {
+    // Import plugin for managing import order and grouping
+    files: ['**/*.{js,jsx}'],
+    plugins: { import: pluginImport },
+    rules: {
+      // Enforce a convention in import order
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin', // Node.js built-ins: fs, path, etc.
+            'external', // Packages from node_modules
+            'internal', // Internal modules (your aliases, paths)
+            'parent', // Imports from parent directories
+            'sibling', // Imports from sibling directories
+            'index', // index files
+            'object', // Imports of objects (rare)
+            'type', // Type imports (TypeScript)
+          ],
+          pathGroups: [
+            {
+              pattern: 'react',
+              group: 'external',
+              position: 'before',
+            },
+          ],
+          pathGroupsExcludedImportTypes: ['react'],
+          'newlines-between': 'never',
+        },
+      ],
     },
   },
 ])
