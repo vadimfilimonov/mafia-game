bb2d0ff55f5ea8b04d7ecaa42fa2c8bf753423c3
Vadim Filimonov <filimonov@duck.com>
2026-03-23 23:30:15 +0300

    Add vite plugin gh pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { ghPages } from "vite-plugin-gh-pages";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/mafia-game/',
   plugins: [
+    ghPages(),
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
